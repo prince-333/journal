848226f24bddd09332c4d0e03f1535e5f6df3d6e
p <lmaopvp26@gmail.com>
2025-01-07 12:11:29 -0600

    Update 3_user_input_exercise_1.py

--- 3_user_input_exercise_1.py
+++ 3_user_input_exercise_1.py
@@ -3,4 +3,4 @@
 length = float(input("What is the length of the rectangle?: "))
 width = float(input("What is the width of the rectangle?: "))
 area = length * width
-print(F"The area is {area} cm²")
+print(f"The area is {area} cm²")
