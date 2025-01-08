a4e6291428ae3544cdc3ece81197c718e698e670
p <lmaopvp26@gmail.com>
2025-01-07 20:12:54 -0600

    Delete 6_if_statements_2.py

--- 6_if_statements_2.py
+++
@@ -1,9 +0,0 @@
-# Another example of "If" statements.
-
-response = input("Would you like food Y/N?: ")
-
-# double == is used for comparisons.
-if response == "Y":
-    print("Have some food! :)")
-else:
-    print("No food for you")
