7f854324946ef7647b4c2f5c6267ba7a3f78b23f
p <lmaopvp26@gmail.com>
2025-01-07 20:11:41 -0600

    Add files via upload

---
+++ Sections 5-9/5_arithmetic_and_math2.py
@@ -0,0 +1,15 @@
+#More examples
+
+import math
+
+x = 9.1
+
+#print(math.pi)
+#print(math.e)
+
+#result = math.sqrt(x)
+#result = math.ceil(x) (Round up 1)
+result = math.floor(x)
+
+
+print(result)
---
+++ Sections 5-9/6_if_statements_3.py
@@ -0,0 +1,7 @@
+# Another example of "If" statements.
+
+name = input("Enter your name: ")
+if name == "":
+    print("You did not type in your name")
+else:
+    print(f"Hello {name}!")
