2356cee05724c1b2c0c59b34e960777aeca9b688
p <lmaopvp26@gmail.com>
2025-01-07 20:13:11 -0600

    Delete 5_arithmetic_and_math_exercise_2.py

--- 5_arithmetic_and_math_exercise_2.py
+++
@@ -1,8 +0,0 @@
-# Area of a circle
-
-import math
-
-radius = float(input("Enter the radius of the circle: "))
-area = math.pi * pow(radius, 2)
-
-print(f"The area of the circle is {round(area, 2)} cm²")
