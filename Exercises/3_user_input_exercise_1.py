982f461a465bfae6d8d89bf4001f32c10b881f64
p <lmaopvp26@gmail.com>
2025-01-07 20:12:19 -0600

    Add files via upload

---
+++ Exercises/3_user_input_exercise_1.py
@@ -0,0 +1,6 @@
+# Exercise 1 Rectangle Area Calculation
+
+length = float(input("What is the length of the rectangle?: "))
+width = float(input("What is the width of the rectangle?: "))
+area = length * width
+print(F"The area is {area} cm²")
---
+++ Exercises/5_arithmetic_and_math_exercise_1.py
@@ -0,0 +1,8 @@
+# Calculate the circumference of a circle
+
+import math
+
+radius = float(input("Enter the radius of a circle: "))
+circumference = 2 * math.pi * radius
+
+print(f"The circumference is {round(circumference, 2)}cm")
