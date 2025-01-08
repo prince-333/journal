2ba8dcd3707890f5e1cc86d3db82e795929d10ea
p <lmaopvp26@gmail.com>
2025-01-07 20:13:51 -0600

    Delete 3_user_input_exercise_1.py

--- 3_user_input_exercise_1.py
+++
@@ -1,6 +0,0 @@
-# Exercise 1 Rectangle Area Calculation
-
-length = float(input("What is the length of the rectangle?: "))
-width = float(input("What is the width of the rectangle?: "))
-area = length * width
-print(f"The area is {area} cm²")
