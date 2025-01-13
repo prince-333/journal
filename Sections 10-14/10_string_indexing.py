bf15d803d0ea9919f9b0fa51ef27ade6b04dcc6e
Prince <princecsantos@gmail.com>
2025-01-12 23:46:45 -0600

    l

---
+++ Sections 10-14/10_string_indexing.py
@@ -0,0 +1,22 @@
+# indexing = accessing elements of a sequence using [] <--- indexing operator
+#           [start : end : step]
+
+credit_number = "1234-5678-9012-3456"
+
+# print(credit_number[1])    will print out first string value
+
+# print(credit_number[0:4])    will print out from x --> y where x is a lower bound value to a higher value y.
+
+# print(credit_number[5:9])   same thing as above
+# print(credit_number[5:])    will print from 5th string to last string in that variable.
+# print(credit_number[-2])    will print background from last string to first string.
+# print(credit_number[::2])   will print everyone second string starting from 1.
+
+# last_digits = credit_number[-4:]       this will print out the last 4 str values
+# print(f'XXXX-XXXX-XXXX-{last_digits}')
+
+# credit_number = credit_number[::-1]  this will print out the string value but in reverse, 
+#                                      starting with the last string, to the first string
+#print(credit_number)
+
+
--- Sections 5-9/9_string_methods_1.py
+++ Sections 5-9/9_string_methods_1.py
@@ -1,7 +1,7 @@
 # String Methods
 
 name = input('Enter your full name: ')
-#phone_number = input('Enter your phone number: ')
+# phone_number = input('Enter your phone number: ')
 
 # result = len(name)            len function will count the number of characters (including spaces) of a said variable
 # result = name.find('')       This will find the first occurence of a specific string, it will return as a "-1" if it cannot find such character.
@@ -12,6 +12,6 @@ name = input('Enter your full name: ')
 # result = name.isdigit()       boolean that will determine if everything in the string contains digits.
 # result = name.isalpha()       boolean that will determine if a string is only alphabetical characters. Will only be true if no spaces and only characters, else: false.
 # result = phone_number.count('-') this will count how many specific characters are in a string.
-# phone_number = phone_number.replace('-', ' ')   this will replace any certain spring with the desired string you'd like to replace it with, in this case, dashes are replaces with spaces.
+# phone_number = phone_number.replace('-', ' ')  this will replace any certain spring with the desired string you'd like to replace it with, in this case, dashes are replaces with spaces.
 
-#print(result)
+print(name)
