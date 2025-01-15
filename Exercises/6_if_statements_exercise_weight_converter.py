e01948b59d1f8bee63c515c5a7077cc463d0f0b3
Prince <princecsantos@gmail.com>
2025-01-15 00:54:25 -0600

    change

--- Exercises/6_if_statements_exercise_weight_converter.py
+++ Exercises/6_if_statements_exercise_weight_converter.py
@@ -6,10 +6,10 @@ unit = input("Kilograms or Pounds (K or L): ")
 if unit == "K":
     weight = round(weight * 2.205, 1)
     unit = "Lbs."
-if unit == "L":
+    print("Your weight is {} {}".format(weight, unit))
+elif unit == "L":
     weight = round(weight / 2.205, 1)
     unit = "Kgs."
+    print("Your weight is {} {}".format(weight, unit))
 else:
     print("{} is neither (K or L)".format(unit))
-
-print("Your weight is {} {}".format(weight, unit))
